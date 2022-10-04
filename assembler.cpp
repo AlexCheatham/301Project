b487cc1aa4e73d9bd07cbeb02673dd5e6e8c88c8
gs3ff <gs3ff@mathcs04.richmond.edu>
2022-10-03 21:06:33 -0400

    made it so you can add multiple files into the command line

--- assembler.cpp
+++ assembler.cpp
@@ -19,30 +19,31 @@ class Assembler {
 int main(int argc, char* argv[]) {
 
     //Makes sure correct amount of command line arguments
-    if (argc != 2) {
+    if (argc < 2) {
         cerr << "Usage: " << argv[0] << " [input file]" << endl;
         exit(0);
     }    
 
-    //Collects the input file and opens it 
-    string inputFile = argv[1];
-    ifstream myFile(inputFile);
-
     vector<string> lines; //Vector containing the lines of code from the file
     string str; //used for inputting the lines into the vector
 
+    for (int i = 1; i < argc; i++) {
+        //Collects the input file and opens it 
+        string inputFile = argv[i];
+        ifstream myFile(inputFile);
 
-    //Adds lines to vector
-    if (myFile.is_open()) {
-        while(getline(myFile, str)) {
-            if(str.size() > 0) {
-                lines.push_back(str);
+        //Adds lines to vector
+        if (myFile.is_open()) {
+            while(getline(myFile, str)) {
+                if(str.size() > 0) {
+                    lines.push_back(str);
+                }
             }
         }
-    }
 
-    //Closes the input file
-    myFile.close();
+        //Closes the input file
+        myFile.close();
+    }
 
     //Checks to make sure vector has the correct inputs
     for (size_t i = 0; i < lines.size(); i++) { 
