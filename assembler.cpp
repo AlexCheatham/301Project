0608023e2cc5d1bfcc5a430103d37aa347aa3335
gs3ff <gs3ff@mathcs04.richmond.edu>
2022-10-03 20:26:52 -0400

    set up the main method to read a file

--- assembler.cpp
+++ assembler.cpp
@@ -14,5 +14,22 @@ class Assembler {
 }
 
 int main() {
-    
+
+    //Collects the input file and opens it 
+    string inputFile = argv[1];
+    ifstream myFile(inputFile);
+
+    vector<string> lines; //Vector containing the lines of code from the fil
+
+    //Adds lines to vector
+    if (myFile.is_open()) {
+        while(myFile>>a) {
+            myParams.push_back(a);
+        }
+    }
+
+    //Closes the input file
+    myFile.close();
+
+    return 0;
 }
